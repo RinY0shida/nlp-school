cf8b1f56c6b1b375a49e036ef41faefccd4de11f
RinY0shida <tororo1219@gmail.com>
2025-10-15 10:47:48 +0900

    feat reading model

--- chapter_7/60.py
+++ chapter_7/60.py
@@ -1,4 +1,8 @@
-import time
+from gensim.models import KeyedVectors
 
-print("aho")
+def main():
+    negative300_path = "../resources/GoogleNews-vectors-negative300.bin"
+    model = KeyedVectors.load_word2vec_format(negative300_path, binary = True)
 
+if __name__ == "__main__":
+    main()
