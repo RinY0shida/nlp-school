d0e52596df39772c5309766b2338e82198aec2e8
RinY0shida <tororo1219@gmail.com>
2025-10-15 11:25:41 +0900

    feat derivation cosine similarity

--- chapter_7/61.py
+++ chapter_7/61.py
@@ -5,10 +5,14 @@ def main():
     negative300_path = "../resources/GoogleNews-vectors-negative300.bin"
     negative300_model = KeyedVectors.load_word2vec_format(negative300_path, binary = True)
 
-    vector = negative300_model["United_States"]
+    # ベクトルから自分で導出できるけど、素晴らAPIがあるのでエコシステムに乗っからせてもらう
+    united_states_vector = negative300_model["United_States"]
+    us_vector = negative300_model["U.S"]
 
-    print("United_States Vector\n")
-    print(vector)
+    
+
+    cosine_similarity_us_united_states = negative300_model.similarity("U.S", "United_States")
+    print("consine similarity =", cosine_similarity_us_united_states)
 
 if __name__ == "__main__":
     main()
