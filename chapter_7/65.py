5a977a979ee0c0122db04c3f31a6d3096566f302
RinY0shida <tororo1219@gmail.com>
2025-10-18 18:18:14 +0900

    fix

--- chapter_7/65.py
+++ chapter_7/65.py
@@ -14,22 +14,47 @@ def main():
     ans64_file.close()
 
     correct_answers_count = 0
-    count = 0
+    count = 1
     category = ans64_data[0][0]
     ans64_data_len = range(len(ans64_data))
     
-    for i in ans64_data_len:
-        if ans64_data[i][0] != category:
-            accuracy = correct_answers_count / count
-            print(f"{category} accuracy: {accuracy}")
-            category = ans64_data[i][0]
-            correct_answers_count = 0
-            count = 0
+    # for i in ans64_data_len:
+    #     if ans64_data[i][0] != category:
+    #         accuracy = correct_answers_count / count
+    #         print(f"{category} accuracy: {accuracy}")
+    #         category = ans64_data[i][0]
+    #         correct_answers_count = 0
+    #         count = 0
         
-        if ans64_data[i][3] == ans64_data[i][5]:
-            correct_answers_count += 1
+    #     if ans64_data[i][3] == ans64_data[i][5]:
+    #         correct_answers_count += 1
 
-        count += 1
+    #     count += 1
+    # 問題に沿った答えを導出する上でここまできれいにする必要なかった
+
+    count = 0
+    for i in ans64_data_len:
+        if ans64_data[i][0][:4] != "gram":
+            count += 1
+            if ans64_data[i][3] == ans64_data[i][5]:
+                correct_answers_count += 1
+        else:
+            accuracy = correct_answers_count / count
+            print(f"semantic analogy accuracy: {accuracy}")
+            count = 0
+            correct_answers_count = 0
+            break
+        
+    for i in ans64_data_len:
+        if ans64_data[i][0][:4] == "gram":
+            count += 1
+            if ans64_data[i][3] == ans64_data[i][5]:
+                correct_answers_count += 1
+        
+    accuracy = correct_answers_count / count
+    print(f"syntactic analogy accuracy: {accuracy}")
+    
+
     
 if __name__ == "__main__":
     main()
