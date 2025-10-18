839837dabc5dc12221ec6cdeaa97f507def4d3df
RinY0shida <tororo1219@gmail.com>
2025-10-18 17:53:29 +0900

    feat 65.py

--- chapter_7/65.py
+++ chapter_7/65.py
@@ -1,52 +1,35 @@
 from gensim.models import KeyedVectors
 
 def main():
-    # pathを通す
-    negative300_path = "../resources/GoogleNews-vectors-negative300.bin"
-    negative300_model = KeyedVectors.load_word2vec_format(negative300_path, binary = True)
-
-    questions_words_path = "../resources/questions-words.txt"
-
-    # questions-wordsの特定の変数だけ抜き出すのは今後のことを考えるとベターじゃない気がするので、全部一気に読み込ませる
-
-    # TODO(RinYoshida): 関数用意させたほうがいいよね
-    questions_words = []
-    txt_file = open(questions_words_path, "r", encoding="utf-8")
-    for line in txt_file:
+    ans64_data = []
+    ans64_path = "result/ans64.txt"
+    ans64_file = open(ans64_path, "r", encoding = "utf-8")
+    
+    for line in ans64_file:
         line = line.strip()
-        if line.startswith(":"):
-            continue
         words = line.strip().split()
         if words:
-            questions_words.append(words)
+            ans64_data.append(words)
 
-    txt_file.close()
+    ans64_file.close()
 
-    txt_file = open("result/64_result.txt", "w", encoding = "utf-8")
-    for i in range(len(questions_words)):
-        composite_vector = negative300_model[questions_words[i][2]] - negative300_model[questions_words[i][1]] + negative300_model[questions_words[i][3]]
-        similar_result = negative300_model.most_similar(composite_vector, topn = 1)
-        similar_word, similarity = similar_result[0]
-        txt_file.write("capital-common-countries "
-                       + questions_words[i][0]
-                       + " "
-                       + questions_words[i][1]
-                       + " "
-                       + questions_words[i][2]
-                       + " "
-                       + questions_words[i][3]
-                       + " "
-                       + similar_word
-                       + " "
-                       + str(similarity)
-                       + "\n"
-                       )
-
+    correct_answers_count = 0
+    count = 0
+    category = ans64_data[0][0]
+    ans64_data_len = range(len(ans64_data))
+    
+    for i in ans64_data_len:
+        if ans64_data[i][0] != category:
+            accuracy = correct_answers_count / count
+            print(f"{category} accuracy: {accuracy}")
+            category = ans64_data[i][0]
+            correct_answers_count = 0
+            count = 0
         
-#    composite_vector = negative300_model[questions_words[2]] - negative300_model[questions_words[1]] + negative300_model[questions_words[3]]
-    
-#    similar_words_united_states = negative300_model.most_similar(composite_vector, topn = 1)
-    
+        if ans64_data[i][3] == ans64_data[i][5]:
+            correct_answers_count += 1
+
+        count += 1
     
 if __name__ == "__main__":
     main()
